691d56978615b181b6cabc7f7e864f3f220f27ad
HP <n>
2021-06-23 02:27:28 +0500

    fetching_refined

--- app/src/main/java/com/example/busbooking/Userhelper/TicketAdapter.java
+++ app/src/main/java/com/example/busbooking/Userhelper/TicketAdapter.java
@@ -1,5 +1,6 @@
 package com.example.busbooking.Userhelper;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,53 +16,78 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class TicketAdapter extends FirebaseRecyclerAdapter<BusModelclass,TicketAdapter.TicketviewHolder> {
+import java.util.ArrayList;
+import java.util.List;
 
-    public TicketAdapter(@NonNull FirebaseRecyclerOptions<BusModelclass> options) {
-        super(options);
+public class TicketAdapter extends RecyclerView.Adapter<TicketAdapter.TicketviewHolder> {
+    private ArrayList<BusModelclass> busList;
+    private Context context;
+    private itemSelected myticket;
+    public TicketAdapter(Context context, ArrayList<BusModelclass> list){
+
+        this.busList=list;
+        this.myticket= (itemSelected) context;
 
     }
 
-    @Override
-    protected void onBindViewHolder(@NonNull TicketAdapter.TicketviewHolder holder, int position, @NonNull BusModelclass model) {
-        holder.tvlocationticket.setText(model.getFromlocation());
-        holder.tvmovtime.setText(model.getStarttiming());
-        holder.tvdestinticket.setText(model.getArrivelocation());
-        holder.tvarrivetime.setText(model.getArrivetiming());
-        holder.tvticketprice.setText(model.getPrice());
-        holder.tvshowTime.setText(model.getStarttiming());
-        holder.tv_bustype.setText(model.getBusCondition());
-
-
+    public interface itemSelected{
+        public void onClickball(int index);
     }
 
     @NonNull
     @Override
-    public TicketviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public TicketviewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.sample_ticketrv,parent,false);
         return new TicketviewHolder(view);
     }
 
-    public class TicketviewHolder extends RecyclerView.ViewHolder{
-        TextView tvlocationticket;
-        TextView tvmovtime;
-        TextView tvdestinticket;
-        TextView tvarrivetime;
-        TextView tvticketprice;
-        TextView tvshowTime;
-        TextView tv_bustype;
-        Button btn_buynow;
+    @Override
+    public void onBindViewHolder(@NonNull  TicketAdapter.TicketviewHolder holder, int position) {
+        holder.tvlocationticket.setText(busList.get(position).getFromlocation());
+        holder.tvmovtime.setText(busList.get(position).getStarttiming());
+        holder.tvdestinticket.setText(busList.get(position).getArrivelocation());
+        holder.tvarrivetime.setText(busList.get(position).getArrivetiming());
+        holder.tvticketprice.setText(busList.get(position).getPrice());
+        holder.tvshowTime.setText(busList.get(position).getStarttiming());
+        holder.tv_bustype.setText(busList.get(position).getBusCondition());
 
-        public TicketviewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvlocationticket=itemView.findViewById(R.id.tvlocationticket);
-            tvmovtime=itemView.findViewById(R.id.tvmovtime);
-            tvdestinticket=itemView.findViewById(R.id.tvdestinticket);
-            tvarrivetime=itemView.findViewById(R.id.tvarrivetime);
-            tvticketprice=itemView.findViewById(R.id.tvticketprice);
-            tvshowTime=itemView.findViewById(R.id.tvshowTime);
-            tv_bustype=itemView.findViewById(R.id.tv_bustype);
-            btn_buynow=itemView.findViewById(R.id.btn_buynow);
-        }
+        //setting tag
+        holder.itemView.setTag(busList.get(position));
     }
+
+    @Override
+    public int getItemCount() {
+        return busList.size();
+    }
+
+    public class TicketviewHolder extends RecyclerView.ViewHolder{
+            TextView tvlocationticket;
+            TextView tvmovtime;
+            TextView tvdestinticket;
+            TextView tvarrivetime;
+            TextView tvticketprice;
+            TextView tvshowTime;
+            TextView tv_bustype;
+            Button btn_buynow;
+
+            public TicketviewHolder(@NonNull View itemView) {
+                super(itemView);
+                tvlocationticket=itemView.findViewById(R.id.tvlocationticket);
+                tvmovtime=itemView.findViewById(R.id.tvmovtime);
+                tvdestinticket=itemView.findViewById(R.id.tvdestinticket);
+                tvarrivetime=itemView.findViewById(R.id.tvarrivetime);
+                tvticketprice=itemView.findViewById(R.id.tvticketprice);
+                tvshowTime=itemView.findViewById(R.id.tvshowTime);
+                tv_bustype=itemView.findViewById(R.id.tv_bustype);
+                btn_buynow=itemView.findViewById(R.id.btn_buynow);
+                //getting tag
+
+                btn_buynow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myticket.onClickball(busList.indexOf((BusModelclass) itemView.getTag()));
+                    }
+                });
+            }
+        }
 }
--- app/src/main/java/com/example/busbooking/ticketsbuy.java
+++ app/src/main/java/com/example/busbooking/ticketsbuy.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.busbooking.Userhelper.TicketAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -20,12 +21,14 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ticketsbuy extends AppCompatActivity {
+public class ticketsbuy extends AppCompatActivity implements TicketAdapter.itemSelected{
 
     RecyclerView recyclerView;
-    TicketAdapter ticketAdapter;
+    RecyclerView.Adapter ticketAdapter;
+    //TicketAdapter ticketAdapter;
     DatabaseReference dbref;
-    private List<BusModelclass>listData;
+    private ArrayList<BusModelclass> listData;
+    private ArrayList<BusModelclass> templistData;
 
 
 
@@ -35,76 +38,107 @@ public class ticketsbuy extends AppCompatActivity {
         setContentView(R.layout.activity_ticketsbuy);
         //getting intent
         listData=new ArrayList<>();
+        templistData=new ArrayList<>();
         Intent intent= getIntent();
         String from= intent.getStringExtra("from");
         String to= intent.getStringExtra("destin");
 
-
-      // ArrayList<DataSnapshot> busModelclasses= new ArrayList<>();
         recyclerView=findViewById(R.id.ticket_recycle);
+        recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        Query query=FirebaseDatabase.getInstance().getReference().child("busses");
-       // dbref= FirebaseDatabase.getInstance().getReference().child("busses");
-       // dbref.orderByChild("busid").addValueEventListener(new ValueEventListener() {
-         /*   @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
 
-                    for( DataSnapshot buses: snapshot.getChildren() ){
-                        if (buses.child("fromlocation").equals(from) && buses.child("arrivelocation").equals(to)){
-                            BusModelclass list= buses.getValue(BusModelclass.class);
-                            listData.add(list);
-                        }
 
-                        }
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        //fetching data from firebase
-     /*   DatabaseReference query= (DatabaseReference) FirebaseDatabase.getInstance().getReference()
-                 .child("busses").addValueEventListener(new ValueEventListener() {
+        FirebaseDatabase.getInstance().getReference("busses")
+                .orderByChild("fromlocation")
+                .equalTo(from)
+                .addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                        listData.clear();
+                        if(snapshot.exists()){
+                            for (DataSnapshot snap : snapshot.getChildren()) {
+                                BusModelclass bus = snap.getValue(BusModelclass.class);
+                                if(bus.arrivelocation.equals(to)){
 
-                        for( DataSnapshot buses: snapshot.getChildren() ){
-
-                            if (buses.child("fromlocation").equals(from) && buses.child("arrivelocation").equals(to)){
-
-                               // busModelclasses.add(buses);
+                                    listData.add(bus);
+                                }
                             }
-                        }
+                             /*   for(int i=0;i<listData.size();i++){
+                                    if(listData.get(i).arrivelocation.equals("to")){
+                                        templistData.add(listData.get(i));
+                                    }
+                                    ticketAdapter.notifyDataSetChanged();*/
+
+                                //Toast.makeText(ticketsbuy.this, listData.get(0).getBusCondition(), Toast.LENGTH_SHORT).show();
+                            }
+                           ticketAdapter.notifyDataSetChanged();
+
+                    /*    FirebaseDatabase.getInstance().getReference()
+                                .child("Busses")
+                                .orderByChild("arrivelocation")
+                                .equalTo(to)
+                                .addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                                        listData.clear();
+                                        if(snapshot.exists()) {
+                                            for (DataSnapshot snap : snapshot.getChildren()) {
+                                                BusModelclass bus = snap.getValue(BusModelclass.class);
+                                                //if(bus.fromlocation.equals("from")) {
+                                                    listData.add(bus);
+                                                //}
+
+                                            }
+                                            ticketAdapter.notifyDataSetChanged();
+                                        }
+                                    }
+
+
+
+                                    @Override
+                                    public void onCancelled(@NonNull  DatabaseError error) {
+                                        Toast.makeText(ticketsbuy.this, error.toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });*/
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
-
+                        Toast.makeText(ticketsbuy.this, error.toString(), Toast.LENGTH_SHORT).show();
                     }
-                });;*/
-
-        FirebaseRecyclerOptions<BusModelclass> options=
-                new FirebaseRecyclerOptions.Builder<BusModelclass>()
-                .setQuery(query,BusModelclass.class)
-                .build();
-         ticketAdapter= new TicketAdapter(options);
+                });
+        ticketAdapter= new TicketAdapter(this, listData);
         recyclerView.setAdapter(ticketAdapter);
 
 
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listData.clear();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                        BusModelclass bus = dataSnapshot.getValue(BusModelclass.class);
+                        listData.add(bus);
+                    }
+                    ticketAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        };
+
+
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        ticketAdapter.startListening();
-    }
+
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        ticketAdapter.stopListening();
+    public void onClickball(int index) {
+       // FirebaseDatabase.getInstance().getReference().child("busses").orderByChild("")
+        Toast.makeText(this, "selected", Toast.LENGTH_SHORT).show();
     }
 }
